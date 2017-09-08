f9014da83b34c036e28cda5a73714e6fed8eea85
Braun Goodson <braun.goodson@digitaslbi.com>
2017-09-07 20:55:33 -0400

    wired up vuex; updated component methods with some-resources data acquisition

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -17,37 +17,89 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <h2>Some Resources</h2>
+    <b-table :data="someResources">
+
+      <template scope="props">
+          <b-table-column label="RID" width="60" numeric>
+              {{ props.row.rid }}
+          </b-table-column>
+
+          <b-table-column label="Name">
+              {{ props.row.rname }}
+          </b-table-column>
+      </template>
+
+      <template slot="empty">
+          <section class="section">
+              <div class="content has-text-grey has-text-centered">
+                  <p>
+                      <b-icon
+                          icon="sentiment_very_dissatisfied"
+                          size="is-large">
+                      </b-icon>
+                  </p>
+                  <p>Nothing here.</p>
+              </div>
+          </section>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import { mapGetters, mapActions } from 'vuex'
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'someResources'
+      ])
+    },
+    methods: {
+      ...mapActions([
+        'fetchResource'
+      ]),
+      async fetch () {
+        this.busy = true
+        await this.fetchResource({
+          url: '/api/v1/some-resources',
+          prop: 'someResources'
+        })
+        this.busy = false
+      }
+    },
+    async created () {
+      this.fetch()
+    },
+    mounted () {
+      console.log('* Mounted Hello Component!')
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
